5e1fb78723b8996c4f636af8cf6427b52d9c3dc6
Mike Jung <themikejung@gmail.com>
2022-06-10 16:49:03 -0700

    uibuild

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
 
-
 app.use(express.json());
 morgan.token('postData', (req, res) => {
   if (req.method === 'POST' || req.method === 'PUT') {
@@ -20,39 +19,6 @@ app.use(morgan(':method :url :status :response-time ms :postData'));
 app.use(express.static('build'));
 app.use(cors());
 
-// let notes = [
-//   {
-//     id: 1,
-//     content: "HTML is easy",
-//     date: "2022-05-30T17:30:31.098Z",
-//     important: true
-//   },
-//   {
-//     id: 2,
-//     content: "Browser can execute only Javascript",
-//     date: "2022-05-30T18:39:34.091Z",
-//     important: false
-//   },
-//   {
-//     id: 3,
-//     content: "GET and POST are the most important methods of HTTP protocol",
-//     date: "2022-05-30T19:20:14.298Z",
-//     important: true
-//   }
-// ]
-
-const generateId = () => {
-  const maxId = notes.length > 0
-  ? Math.max(...notes.map(n => n.id))
-  : 0
-
-  return maxId + 1
-}
-
-// app.get('/', (request, response) => {
-//   response.send('<h1>Hello World</h1>')
-// })
-
 app.get('/api/notes', (request, response) => {
   // response.json(notes);
   Note
@@ -63,20 +29,17 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find(note => note.id === id)
-  if (note) {
-    response.json(note);
-  } else {
-    response.status(404).end();
-  }
-})
+  Note
+    .findById(request.params.id)
+    .then(note => response.json(note))
+  })
 
 app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter( note => note.id !== id)
-
-  response.status(204).end();
+  const id = request.params.id;
+  Note
+    .deleteOne({_id: id})
+    .then(response.sendStatus(204).end())
+  // response.status(204).end();
 })
 
 app.post('/api/notes', (request, response) => {
@@ -87,24 +50,32 @@ app.post('/api/notes', (request, response) => {
     })
   }
 
-  const note = {
-    id: generateId(),
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    date: new Date().toISOString(),
-  }
+    date: new Date()
+  })
 
-  notes = notes.concat(note);
-
-  response.json(note)
+  note
+    .save()
+    .then(savedNote => {
+      response.json(savedNote)
+    });
 })
 
 app.put('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id);
+  const id = request.params.id;
   const newNote = request.body;
-  notes = notes.map(n => n.id !== id ? n : newNote)
-  console.log(notes);
-  response.json(newNote);
+  Note
+    .findById(id)
+    .then(note => {
+      note.content = newNote.content;
+      note.important = newNote.important;
+      return note.save()
+    })
+    .then(note => {
+      response.json(note)
+    })
 })
 
 const unknownEndpoint = (request, response) => {
